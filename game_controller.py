c7ce3887792034822773281647507efca0f3d579
ShinsekaiXD <underfellsans185@gmail.com>
2025-09-30 11:22:38 +0300

    Changes to be committed: 	modified:   game_controller.py

--- game_controller.py
+++ game_controller.py
@@ -3,6 +3,10 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((600, 400))
 clock = pygame.time.Clock()
+mouse_pressed = False
+mouse_pos = pygame.mouse.get_pos()
+
+
 class Character:
     def __init__(self, hp, atk):
         self.hp = hp
@@ -15,25 +19,31 @@ class Button:
         self.action = action
 
     def draw(self, x, y):
-        mouse = pygame.mouse.get_pos()
-        click = pygame.mouse.get_pressed()
-
-        if (x < mouse[0] < x + self.width) and (y < mouse[1] < y + self.height):
+        if (x < mouse_pos[0] < x + self.width) and (y < mouse_pos[1] < y + self.height):
             pygame.draw.rect(screen, (23, 204, 58), (x, y, self.width, self.height))
-                
-            if click[0] == 1 and self.action != None:
+    
+            if self.action != None and mouse_pressed:
                 self.action()
-
+                
         else:
             pygame.draw.rect(screen, (13, 162, 58), (x, y, self.width, self.height))
 
+def increment():
+    n = 0
+    print(n)
+    n += 1
+
 def game_state():
     is_running = True
-    button = Button(100, 50)
+    button = Button(100, 50, increment)
     while is_running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 is_running = False
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                mouse_pressed == True
+            if event.type == pygame.MOUSEBUTTONUP and event.button == 1 :
+                mouse_pressed == False
         screen.fill((20, 20, 20))
         button.draw(20, 100)
         pygame.display.update()
